118567fe9cbb6c64068076062d3ebb7b2e56cb0e
Emmanuel Ajako <137904655+emmanuelajako406@users.noreply.github.com>
2024-06-28 09:48:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 3000,
   wrap: true
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
